5e0020c606fe834f4252f157c91ea777e43ac28e
martamaria96 <maria.ldias96@gmail.com>
2019-04-21 12:56:53 +0100

    trainwnet

--- train_wnet.py
+++ train_wnet.py
@@ -5,44 +5,51 @@ from clr_callback import *
 import os.path
 import numpy as np
 import tifffile as tiff
-import rasterio
 import glob
 import gc
-
+import tensorflow as tf
 from keras.callbacks import CSVLogger
 from keras.callbacks import TensorBoard
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
+sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
 N_BANDS = 3
 N_CLASSES = 6  # imp surface, car, building, background, low veg, tree
 N_EPOCHS = 50
-#N_EPOCHS = 5
 UPCONV = True
-PATCH_SZ = 128   # should divide by 16
-BATCH_SIZE = 50
-STEPS_PER_EPOCH = 10000
-#STEPS_PER_EPOCH = 10
-VALIDATION_STEPS = 2400
-#VALIDATION_STEPS = 6
-MAX_QUEUE = 10
 
-TRAIN_IDS = ['2_10','2_11','3_10','3_11','4_10','4_11','5_10','5_11','6_7','6_8','6_9','6_10','6_11','7_7','7_8','7_9','7_10','7_11']
-VAL_IDS = ['2_12','3_12','4_12','5_12','6_12','7_12']
-#TRAIN_IDS = ['2_10']
-#VAL_IDS = ['2_12']
+dataset = 'p'
+load_unet = True
+if dataset == 'p':
+    TRAIN_IDS = ['2_10','2_11','3_10','3_11','4_10','4_11','5_10','5_11','6_7','6_8','6_9','6_10','6_11','7_7','7_8','7_9','7_10','7_11']
+    VAL_IDS = ['2_12','3_12','4_12','5_12','6_12','7_12']
+    path_img = '/home/mdias/deep-wnet/potsdam/Images_lab/top_potsdam_{}_RGB.tif'
+    path_mask = '/home/mdias/deep-wnet/potsdam/Masks/top_potsdam_{}_label.tif'
+    PATCH_SZ = 320   # should divide by 16
+    BATCH_SIZE = 12
+    STEPS_PER_EPOCH = 10000
+    VALIDATION_STEPS = 2400
+    MAX_QUEUE = 10
+elif dataset == 'v':
+    TRAIN_IDS = ['1', '3', '11', '13', '15', '17', '21', '26', '28', '30', '32', '34']
+    VAL_IDS = ['5', '7', '23', '37']
+    path_img = '/tmp/vaihingen/Images_lab/top_mosaic_09cm_area{}.tif'
+    path_mask = '/tmp/vaihingen/Masks/top_mosaic_09cm_area{}.tif'
+    PATCH_SZ = 320   # should divide by 16
+    BATCH_SIZE = 15
+    STEPS_PER_EPOCH = 4000
+    VALIDATION_STEPS = 1000
+    MAX_QUEUE = 10
 
-path_img = '/tmp/potsdam/Images/top_potsdam_{}_RGB.tif'
-path_mask = '/home/mdias/deep-wnet/potsdam/Masks/top_potsdam_{}_label.tif'
 
-#path_img = './../data-mdias/Images/top_potsdam_{}_RGB.tif'
-#path_mask = './../data-mdias/Masks/top_potsdam_{}_label.tif'
 
 def get_model():
   model = wnet_model(N_CLASSES, PATCH_SZ, n_channels=N_BANDS, upconv=UPCONV)
   return model
 
-weights_path = 'weights_wnet_norm'
+weights_path = 'weights_wnet2'
 if not os.path.exists(weights_path):
     os.makedirs(weights_path)
 
@@ -57,7 +64,7 @@ if __name__ == '__main__':
             model = get_model()
             if os.path.isfile(wnet_weights):
                 model.load_weights(wnet_weights)
-            model.load_weights( 'weights_unet/unet.hdf5', by_name = True)
+            model.load_weights( 'weights_unet2/unet_weights.hdf5', by_name = True)
             early_stopping = EarlyStopping(monitor='val_loss', restore_best_weights = True, patience = 5, mode ='min')
             model_checkpoint = ModelCheckpoint(wnet_weights, monitor='val_loss', save_best_only=True, mode = 'min')
             csv_logger = CSVLogger('log_unet.csv', append=True, separator=';')
