0b7099b9ddfb09201bf3955fdc2236b915ceea4d
martamaria96 <maria.ldias96@gmail.com>
2019-05-07 16:20:04 +0100

    new structure

--- scripts_history/predict_all.py
+++
@@ -1,5 +0,0 @@
-from predict import predict_all
-steps = [16, 8, 4, 2, 1]
-for i in steps:
-    print('\n\n',i)
-    predict_all(i)
--- scripts_history/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-nohup python3 train_unet_generator.py &> train_resize.out&
-nohup python3 predict.py &> predict.out&
-
-nohup sh -c 'python3 train_unet.py >train.out && python3 predict.py >predict.out' &
